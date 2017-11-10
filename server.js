0c33502ca4840f1f56d9425aa7446fb10ac95576
Kimberly Kearney <kimcheru18@gmail.com>
2017-11-09 17:21:34 -0800

    added more to html, updated server.js

--- server.js
+++ server.js
@@ -1,13 +1,40 @@
-const express = require('express');
+var express = require('express');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var config = require('./config');
 const morgan = require('morgan');
 
 const app = express();
-
-
-
 app.use(express.static('public'));
+app.use(bodyParser.json());
 
 app.use(morgan('common'));
 
 
-app.listen(process.env.PORT || 8080);
+var runServer = function (callback) {
+    mongoose.connect(config.DATABASE_URL, function (err) {
+        if (err && callback) {
+            return callback(err);
+        }
+
+        app.listen(config.PORT, function () {
+            console.log('Listening on localhost:' + config.PORT);
+            if (callback) {
+                callback();
+            }
+        });
+    });
+};
+
+if (require.main === module) {
+    runServer(function (err) {
+        if (err) {
+            console.error(err);
+        }
+    });
+};
+
+exports.app = app;
+exports.runServer = runServer;
+
+app.listen(process.env.PORT);
