e152938f3f26593af50f9514df4a36c0ead4e7ab
Kimberly Kearney <kimcheru18@gmail.com>
2017-11-11 14:37:29 -0800

    updated css, html, added some js

---
+++ public/js/client.js
@@ -0,0 +1,68 @@
+//1. User selects 'Let's begin'
+//2. User enters answer in answer field
+//3. User clicks 'Next'
+//4. From second question on, user has the option to click 'Back' or 'Next'
+//5.
+
+var questionsArray = [
+    //Question 1
+        {
+            questionText: 'What brings me true happiness in life?',
+            questionChoices: questionCorrectChoice: correctDetails:
+    },
+
+    //Question 2
+        {
+            questionText: 'What are my main personality traits?',
+            questionChoices: questionCorrectChoice: correctDetails:
+    },
+
+    //Question 3
+        {
+            questionText: 'Who do I respect and what and what qualities do I see in them that I would like to emulate?',
+            questionChoices: questionCorrectChoice: correctDetails:
+    },
+
+    //Question 4
+        {
+            questionText: 'What strengths do I posses?',
+            questionChoices: questionCorrectChoice: correctDetails:
+    },
+
+    //Question 5
+        {
+            questionText: 'What are my weaknesses?',
+            questionChoices: questionCorrectChoice: correctDetails:
+    },
+
+
+    //Question 6
+        {
+            questionText: 'If I could do anything with my time and money wasn’t an object, what would that look like?',
+            questionChoices: questionCorrectChoice: correctDetails:
+    },
+
+
+    //Question 7
+        {
+            questionText: 'What do I want to have accomplished by the end of my life?',
+            questionChoices: questionCorrectChoice: correctDetails:
+    },
+
+    //Question 8
+        {
+            questionText: 'What will I regret not doing?',
+            questionChoices: questionCorrectChoice: correctDetails:
+    },
+
+    //Question 9
+        {
+            questionText: 'What are my core values?',
+            questionChoices: questionCorrectChoice: correctDetails:
+    },
+
+    //Question 10
+        {
+            questionText: 'What are my beliefs??',
+            questionChoices: questionCorrectChoice: correctDetails:
+    },
