7c2a2fc17aaa234e348d33999039f9e26225d9cc
Kimberly Kearney <kimcheru18@gmail.com>
2017-11-13 15:00:00 -0800

    added some js, updated css and html

--- public/js/client.js
+++ public/js/client.js
@@ -63,6 +63,64 @@ var questionsArray = [
 
     //Question 10
         {
-            questionText: 'What are my beliefs??',
+            questionText: 'What are my beliefs?',
             questionChoices: questionCorrectChoice: correctDetails:
     },
+
+
+    var currentQuestionNumber = 0;
+    var totalNumberOfQuestions = quesstionsArray.length;
+
+//    Step 2. defining functions
+
+//    display description info (Your assignment, why create a SOP) along with "Let's get started"
+    function displayDescription() {
+
+    }
+
+
+    // display question text that requires single essay answer
+    function displayQuestionSingle() {
+            $('.question').text(questionsArray[currentQuestionNumber].questionText);
+    }
+    // 1. user types answer in input field
+    // 2. on 'click' user submits answer
+    // 3. next question appears (repeat 1 and 2)
+
+
+    // display question text that requires multiple answers
+    function displayQuestionMulti() {
+            $('.values-beliefs').text(questionsArray[currentQuestionNumber].questionText);
+    }
+
+    //display "Review Your Answers" text
+
+    //display questions with users answers
+
+    //display option to "Create SOP" free-style or use Template
+
+    //display SOP, My Values, My Beliefs, My Goals
+
+    //always display navigation bar
+
+    //clicking on links will display links to other web pages with more information
+
+// Step 3. Using functions
+$(document).ready(function () {
+
+            //on page load, hide questions, completed-questions, create-sop, completed-sop-vbg, keep intro section only
+            $('.questions').hide();
+            $('.completed-questions').hide();
+            $('.create-sop').hide();
+            $('.completed-sop-vbg').hide();
+            //start button trigger (onClick) in order to hide the intro container and display the question container
+            $('.start-button').click(function () {
+                $('.welcome-page').hide();
+                $('.description-info').hide();
+                $('.questions').show();
+                $('.completed-questions').hide();
+                $('.create-sop').hide();
+                $('.completed-sop-vbg').hide();
+                displayQuestionSingle();
+            });
+        });
